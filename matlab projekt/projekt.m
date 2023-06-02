d3cf0b4938a3bcfefb33b0557ba63484a034f282
Dominik319 <84710359+Dominik319@users.noreply.github.com>
2023-06-02 19:29:45 +0200

    Update projekt.m

--- matlab projekt/projekt.m
+++ matlab projekt/projekt.m
@@ -1,6 +1,6 @@
 %parametry modelu
-dT = 20;
-Tsym = 2000;
+dT = 1;
+Tsym = 100;
 p=0.1;
 Hu = 20;
 Hy = 25;
@@ -10,8 +10,14 @@ n = 3;
 m = 1;
 
 
-a=[1, -1.417, 0.4169];
-b=[0.01191, 0.008914];  %%% Assumes first parameter is zero so excluded
+
+%a=[1, -0.8832, 0]; % stara transmitancja
+%b=[0, 0.005457];
+
+
+a=[1, -0.7292, 0];
+b=[5.762, 6.893];
+%%% Assumes first parameter is zero so excluded
 
 
 
@@ -29,7 +35,7 @@ jedn = zeros([Hu 1]);
 jedn(1,1) = 1;
 pom = -(jedn' * inv(W));
 %zmienne globalne
-Yr = 20* ones([Hy 1]);
+Yr = 42* ones([Hy 1]);
 y = zeros(1, N+Hu);   %macierz wyjsc wszystkich
 u =  zeros(1, N+Hu);  %macierz sterowania w calym zakresie
 du = zeros(1, N+Hu);  %macierz zmian sterowania w calym zakresie
@@ -42,16 +48,18 @@ duk = zeros([Hu 1]);    %dU przewidujace
 Tp = 0:1:N-1+Hu;
 DU0 = 0; %sygnal startowy 
 
+start = 24;
+
 for j = 1:N-1
     switch j
         case 1
-            Yp = [0 ;0 ;0];
+            Yp = [start ;start ;start];
             dul = DU0;
         case 2
-            Yp = [y(1) ;0 ;0];
+            Yp = [y(1) ;start ;start];
             dul = u(1)-DU0;
         case 3
-            Yp = [y(2) ;y(1) ;0];
+            Yp = [y(2) ;y(1) ;start];
             dul = u(2)-u(1);
         otherwise
             Yp = [y(j-1) ;y(j-2) ;y(j-3)];
@@ -66,8 +74,13 @@ for j = 1:N-1
 
     if(j<2)
         for o = 1:1:Hu
-            u(j+o-1) = du(j+o-1);
-            u(j+o) = u(j+o-1) + 0.01*du(j+o-1);    % kolejne du inaczej 
+            if(o==1)
+                u(j+o-1) = du(j+o-1);
+            else
+                u(j+o) = u(j+o-1) + 0.01*du(j+o-1);    % kolejne du inaczej 
+            end
+
+
             if(o == 1)
                 duk(o) =  u(j+o);
             else
@@ -76,7 +89,7 @@ for j = 1:N-1
         end
     else
         for o = 1:1:Hu
-            u(j+o) = u(j+o-1) + du(j+o-1);
+            u(j+o-1) = u(j+o-2) + du(j+o-2);
             duk(o) =  u(j+o)-u(j+o-1);
         end
     end
@@ -84,10 +97,9 @@ for j = 1:N-1
     Yk = H*duk + P1*Yp + P2 * dul;
     y(j) = Yk(1,1);
     subplot(2,1,1)
-    plot(Tp,y,'o');
+    plot(Tp(1,1:100),y(1,1:100),'o');
     subplot(2,1,2)
     plot(Tp(:,1:1:end-Hu),u(:,1:1:end-Hu),'-',Tp(:,1:1:end-Hu),du(:,1:1:end-Hu),'o')
-    pause;
     
 end
 
