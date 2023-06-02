d1e4362c9b98d2072c48f629b1561f3edb3f5935
paweldro <paweldro@tlen.pl>
2023-06-02 19:20:29 +0200

    Update GPC.ino
    połączony algorytm gpc z obsługą sprzętu

--- GPC/GPC.ino
+++ GPC/GPC.ino
@@ -1,4 +1,7 @@
 #include <Wire.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
 #include <elapsedMillis.h>
 #include "konfig.h"
 #include "matrix.h"
@@ -11,6 +14,33 @@
 File myFile;
 
 const int chipSelect = BUILTIN_SDCARD;
+
+
+
+/********************************************************************/
+// Data wire is plugged into pin 8 on the Arduino 
+#define ONE_WIRE_BUS 8 
+const int rs = 12, en = 11, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+float pom;
+float start_pom;
+int odczyt;
+float zadana;
+int odliczanie;
+bool zmiana_zadania;
+bool pierwszy;
+
+int obliczenia;
+float procent; //Obie rzeczy do PWM
+
+
 
 int steps = 0;
 //Deklaracja macierzy
@@ -37,6 +67,7 @@ Matrix V(HU, 1);
 Matrix deltaUwprawo(HU, 1);
 
 Matrix deltaUk(1, 1);
+Matrix deltaUkpoprz(1, 1);
 
 Matrix UK(1, 1);
 Matrix UKminusjeden(1, 1);
@@ -50,9 +81,42 @@ Matrix wyjscie(1, 1);
 Matrix macierzdoprzesuwania(HU, HU);
 void setup() {
     /* serial to display data */
-    Serial.begin(115200);
-    while(!Serial) {}
-    pinMode(LED_BUILTIN, OUTPUT);
+   Serial.begin(9600);
+   while(!Serial) {}
+   pinMode(LED_BUILTIN, OUTPUT);
+   pinMode(A0,INPUT);
+   pinMode(1,INPUT);
+   pinMode(0,OUTPUT);  
+   lcd.begin(16,2);
+   lcd.print("Projekt Grupowy");
+   lcd.setCursor(0,1);
+   lcd.print("8@KSA'2023");
+   zmiana_zadania = false;
+   pierwszy = true;
+   odliczanie = 0;
+ 
+   obliczenia = 0;
+   analogWrite(0,obliczenia); //PWM
+ 
+    
+ // Start up the library 
+ sensors.begin();
+ sensors.requestTemperatures();
+ start_pom = sensors.getTempCByIndex(0);
+
+    wyjscie[0][0] = sensors.getTempCByIndex(0);
+ // Serial.println("wyjscie: ");
+  //Serial.println(wyjscie[0][0]);
+
+  //przesuwanie macierzy Ywlewo
+    for(int i = NA-1; i>-1; i-- )
+    { 
+      
+      Ywlewo[i][0]=wyjscie[0][0];
+      }
+      
+
+
     
     //inicjalizacja karty SD
     Serial.print("Inicjalizacja karty SD: ");
@@ -98,7 +162,7 @@ void setup() {
     //Wyświetlanie macierzy P2
     
     //P2.vPrintFull();
-    float p = 0.5;
+    float p = 0.1;
     W = 2*(H.Transpose()*H+p*I);
     pomocnicza = -1*jed*W.Invers();
 
@@ -112,67 +176,159 @@ void setup() {
       macierzdoprzesuwania[i][i+1]=1;
       
     }
-    //generowanie macierzy YR
-       for(int i = 0; i<HU; i++ ){
-      YR[i][0]=wz;
-      
-    }
-      
-    //YR = YR.Transpose();
-    //macierzdoprzesuwania[HY-1][0]=1;
-   
-    //YR = YR*macierzdoprzesuwania;
-    //YR = YR.Transpose();
 
+    Serial.print("Pomiar:   ");
+    Serial.print("Procent:  ");
+    Serial.print("delta uk:  ");
+    Serial.println("uk:  ");
     
-    /*//przesuwanie macierzy Ywlewo
-    for(int i = NA-1; i>0; i-- )
-    { 
-      Ywlewo[i][0]=Ywlewo[i-1][0];
-      }
-     Ywlewo[0][0]=wyjscie[0][0];*/
-
-    
-    
-    //wyliczanie delta u w lewo deltaUwlewo trzeba dopasować do transmitancji 
-    //deltaUwlewo[0][0] =deltaUk[0][0];
-    
-    //Ywlewo.vPrintFull();
-
-    //obliczanie chwilowego wyjscia - to na końcu
-    //wyjscie=H*deltaUwprawo+P1*Ywlewo+P2*deltaUwlewo;
-
+    UK[0][0] = 0;
+    deltaUkpoprz[0][0] = 0;
     
    }
   
 
 void loop() {
   //loop
-  if(steps < 40)
-  {
+
+
+
+sensors.requestTemperatures(); // Send the command to get temperature readings 
+ while(pierwszy){
+     odczyt = analogRead (A0);
+      //zadana = (((odczyt-27.00)/1000.00)*(65.00-start_pom))+start_pom;
+      zadana = 47.7;
+      if(zadana<start_pom){
+        //zadana = start_pom;
+        zadana = 47.7;
+      }
+     odliczanie = odliczanie+1;
+     lcd.clear();
+     lcd.setCursor(0,0); 
+     lcd.print("Zadawana T= ");
+     for(int i = 0; i<HU; i++ ){
+      YR[i][0]=zadana;
+      
+    }
+     lcd.print(zadana);
+     lcd.setCursor(0,1);
+     lcd.print(10-odliczanie);
+     if(odliczanie==10){
+      pierwszy = false;
+      }
+      delay(1000);
+ }
+
+
+ if(digitalRead(1) == HIGH){
+    while(digitalRead(1) == HIGH){
+      odczyt = analogRead (A0);
+      //zadana = (((odczyt-27.00)/1000.00)*(65.00-start_pom))+start_pom;
+      zadana = 47.7;
+      if(zadana<start_pom){
+        //zadana = start_pom;
+        zadana = 47.7;
+      }
+      lcd.clear(); 
+      lcd.print("Zadawana T= ");
+      lcd.print(zadana);
+
+          //generowanie macierzy YR
+       for(int i = 0; i<HU; i++ ){
+      YR[i][0]=zadana;
+      
+    }
+      delay(1000);
+      }
+  }
+
+ 
+ 
+ lcd.clear(); 
+ lcd.setCursor(0,0);
+ lcd.print("Zadana T= ");
+ lcd.print(zadana);
+ lcd.print("C");
+ lcd.setCursor(0,1);
+ lcd.print("Obecna T= ");
+ lcd.print(sensors.getTempCByIndex(0));
+ lcd.print("C");
+
+  pom = sensors.getTempCByIndex(0);
+    
+  
   V = -2*H.Transpose()*(YR-P1*Ywlewo-P2*deltaUwlewo); //obliczanie wartosci V
   deltaUk = pomocnicza*V; //obliczanie wartosci delta Uk
+  //Serial.println("deltauk: ");
+ // Serial.println(deltaUk[0][0]);
+
 
   
+
   deltaUwprawo[0][0]= deltaUk[0][0]; //zweryfikowc bo wpisujemy tylko pierwsza wartosc, reszta to zera
   
-  wyjscie=H*deltaUwprawo+P1*Ywlewo+P2*deltaUwlewo; //obliczanie chwilowego wyjscia
+  //wyjscie=H*deltaUwprawo+P1*Ywlewo+P2*deltaUwlewo; //obliczanie chwilowego wyjscia
+  wyjscie[0][0] = sensors.getTempCByIndex(0);
+ // Serial.println("wyjscie: ");
+  //Serial.println(wyjscie[0][0]);
 
-  //przesuwanie
   //przesuwanie macierzy Ywlewo
     for(int i = NA-1; i>0; i-- )
     { 
       Ywlewo[i][0]=Ywlewo[i-1][0];
       }
-     Ywlewo[0][0]=wyjscie[0][0];
+       Ywlewo[0][0]=wyjscie[0][0];
    
    //wyliczanie delta u w lewo deltaUwlewo trzeba dopasować do transmitancji 
     deltaUwlewo[0][0] =deltaUk[0][0];
-    Serial.println(wyjscie[0][0]);
-    //wyjscie.vPrintFull();
-    delay(1000);
-    steps = steps + 1;
-  }
+   
+
+
+
+    
+    //procent = (2.6408*deltaUk[0][0])-73.3237; // z temp na % wypelnienia
+// if(deltaUk[0][0]<=start_pom){
+  //  procent = 0;
+//}else{
+    deltaUkpoprz[0][0] = UK[0][0];
+    UK[0][0]= UK[0][0]+deltaUk[0][0];
+    
+    
+    procent = UK[0][0];
+    //procent = (0.1241*(UK[0][0])-9.1556);
+    if(procent<0)
+    {
+      procent = 0;
+      UK[0][0] = 0;
+      deltaUk[0][0] =UK[0][0]- deltaUkpoprz[0][0];
+      
+    }
+    if(procent>100){
+      procent = 100;
+      UK[0][0] = 100;
+      deltaUk[0][0] =UK[0][0]- deltaUkpoprz[0][0];
+      
+    }
+//}
+ // wyjscie=H*deltaUwprawo+P1*Ywlewo+P2*deltaUwlewo; //obliczanie chwilowego wyjscia
+  //Serial.println("UK: ");
+  //Serial.println(UK[0][0]);
+   Serial.print(wyjscie[0][0]);
+   Serial.print("     ");
+    Serial.print(procent);
+    Serial.print("      ");
+    Serial.print(deltaUk[0][0]);
+    Serial.print("      ");
+    Serial.println(UK[0][0]);
+ obliczenia = (procent/100)*255; // z % na syg 0-255
+ analogWrite(0,obliczenia); // wyjscie
+  // Serial.println("procent: ");
+  //Serial.println(procent); 
+  // delay(100); 
+  delay(60000);
+
+
+    
 }
 
 
@@ -197,7 +353,7 @@ void readmatrix(char const *filename, Matrix &matrix, int x, int y)
     }
   }  
   else {
-    Serial.println("Nie można otworzyć pliku: " + String(filename));
+   Serial.println("Nie można otworzyć pliku: " + String(filename));
    
   }       
  
@@ -212,7 +368,7 @@ void SPEW_THE_ERROR(char const * str)
     #if (SYSTEM_IMPLEMENTATION == SYSTEM_IMPLEMENTATION_PC)
         cout << (str) << endl;
     #elif (SYSTEM_IMPLEMENTATION == SYSTEM_IMPLEMENTATION_EMBEDDED_ARDUINO)
-        Serial.println(str);
+        //Serial.println(str);
     #else
         /* Silent function */
     #endif
